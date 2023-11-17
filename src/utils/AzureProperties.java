41e23a08c62a163d5696564f5cec0f7e74abf52f
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-11-17 06:32:02 +0000

    Set local keys

--- src/utils/AzureProperties.java
+++ src/utils/AzureProperties.java
@@ -1,6 +1,11 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class AzureProperties {
+    private static final String PROPS_FILE = "azurekeys-westeurope.props";
     private static final String BLOB_KEY = "BlobStoreConnection";
     private static final String COSMOSDB_KEY = "COSMOSDB_KEY";
     private static final String COSMOSDB_URL = "COSMOSDB_URL";
@@ -11,8 +16,8 @@ public class AzureProperties {
     public static String DB_KEY = "";
     public static String DB_NAME = "";
     public static String STORAGE_CONNECTION_STRING = "";
-    public static String CACHE_HOSTNAME = "rediswesteurope68881.redis.cache.windows.net";
-    public static String CACHE_KEY = "VtDyZorArQ0rbLFB43uconCp5blgLF1L1AzCaDJUCP8=";
+    public static String CACHE_HOSTNAME = "";
+    public static String CACHE_KEY = "";
 
     public AzureProperties() {}
 
@@ -24,4 +29,22 @@ public class AzureProperties {
         CACHE_HOSTNAME = System.getenv(REDIS_URL);
         CACHE_KEY = System.getenv(REDIS_KEY);
     }
+
+    public static void setLocalKeys() {
+        Properties props = new Properties();
+        try {
+            props.load( new FileInputStream(PROPS_FILE));
+        } catch (IOException e) {
+            // do nothing
+        }
+
+        CONNECTION_URL = props.getProperty(COSMOSDB_URL);
+        DB_KEY = props.getProperty(COSMOSDB_KEY);
+        DB_NAME = props.getProperty(COSMOSDB_DATABASE);
+        STORAGE_CONNECTION_STRING = props.getProperty(BLOB_KEY);
+        CACHE_HOSTNAME = props.getProperty(REDIS_URL);
+        CACHE_KEY = props.getProperty(REDIS_KEY);
+    }
+
+
 }
